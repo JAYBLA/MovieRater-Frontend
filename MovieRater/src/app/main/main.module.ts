2d2f86b2a27c3b7bbba9a9001810046bef0100b4
JAYBLA <jayblaenterprises@gmail.com>
2021-06-05 11:31:04 +0300

    main sub components and styling the app

--- MovieRater/src/app/main/main.module.ts
+++ MovieRater/src/app/main/main.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import {Routes, RouterModule} from '@angular/router';
 
 import { MainComponent } from './main.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { MovieFormComponent } from './movie-form/movie-form.component';
 
 const routes: Routes=[
   {path: 'movies', component:MainComponent}
@@ -11,7 +14,7 @@ const routes: Routes=[
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MovieListComponent, MovieDetailsComponent, MovieFormComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
